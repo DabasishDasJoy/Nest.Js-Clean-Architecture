068ecf90ad42dafddbeaa05611f8161171d52f08
Dabasish Das Joy <dabasishdasjoy92@gmail.com>
2025-08-02 22:16:49 +0600

    interface binding to the user repository

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'generated/prisma';
 import { PrismaService } from 'src/common/database/prisma.service';
+import { IUserRepository } from '../interfaces/user-repository.interface';
 
 @Injectable()
-export class UserRepository {
+export class UserRepository implements IUserRepository{
   constructor(private readonly prisma: PrismaService) {}
 
   async getUsers(): Promise<User[]> {
