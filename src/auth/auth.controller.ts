8561166e753be670a93e590e7cc0170ae1864ab8
Dabasish Das Joy <dabasishdasjoy92@gmail.com>
2025-08-16 21:41:46 +0600

    register controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    
+    @Post('register')
+    registerV1(@Body() body: any): any{
+        
+        return { message: 'User registered successfully', data: body };
+    }
+}
