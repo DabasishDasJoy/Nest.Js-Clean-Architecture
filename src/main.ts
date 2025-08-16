ab1fb88d49654d75d921d173ae3b63c3e486f246
Dabasish Das Joy <dabasishdasjoy92@gmail.com>
2025-08-16 21:33:02 +0600

    api versioning

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { IServerConfig, IDatabaseConfig } from './common/config/index';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
     const logger = new Logger('Bootstrap');
@@ -15,6 +15,18 @@ async function bootstrap() {
     if (!serverConfig || !dbConfig) {
         throw new Error('Configuration not found');
     }
+
+    app.setGlobalPrefix(serverConfig.apiPrefix);
+    app.enableCors({
+        origin: serverConfig.corsOrigin,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    })
 
     await app.listen(serverConfig.port, serverConfig.host);
     logger.log(
--- src/user/showing/showing.service.ts
+++ src/user/showing/showing.service.ts
@@ -2,30 +2,16 @@ import { Inject, Injectable } from '@nestjs/common';
 import { IUserRepository } from '../interfaces/user-repository.interface';
 import { REPOSITORY_TOKENS } from 'src/common/tokens/repository.tokens';
 import { IUserShowingService } from '../interfaces/user-showing-service.interface';
+import { UserRepository } from '../repositories/user.repository';
 
 @Injectable()
 export class ShowingService implements IUserShowingService {
     constructor(
         @Inject(REPOSITORY_TOKENS.USER)
-        private readonly userRepository: IUserRepository,
+        private readonly userRepository: UserRepository,
     ) {}
 
-<<<<<<< HEAD
     async showAllUsers() {
         return await this.userRepository.getUsers();
     }
-=======
-  async showAllUsers() {
-    // Fetch all users using the user repository
-    // and return the result.
-    // This method is used by the UserController
-    // to respond to the 'list' endpoint.
-    // It returns a promise that resolves to an array of User objects.
-    // The UserRepository's getUsers method is called to retrieve the users.
-    // This service is injected into the UserController.
-    // The UserController uses this service to handle the 'list' endpoint.
-    // The service is responsible for business logic related to showing users.
-    return await this.userRepository.getUsers();
-  }
->>>>>>> b8046a437ad159702b77cd4826611261a90a3ba1
 }
